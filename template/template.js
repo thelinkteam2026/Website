205855e38d6d03ff80ee5b539f8cdd2587a93a2b
FramelinK <thelinkteam2026@gmail.com>
2026-08-18 14:26:14 +0200

    Update template.js

--- template/template.js
+++ template/template.js
@@ -1,12 +1,17 @@
 const splash = document.querySelector('.splash');
+const sideBar = document.querySelector(".sidebar");
+console.log(sideBar);
 
-document.addEventListener('DOMContentLoaded', (e)=>{
-    setTimeout(()=>{
+document.addEventListener('DOMContentLoaded', (e) => {
+    setTimeout(() => {
         splash.classList = "fade";
     }, 2000)
 });
 function showSideBar() {
-    const sideBar = document.querySelector(".sidebar");
     sideBar.style.display = "flex";
-    
+}
+function hideSideBar() {
+    event.stopPropagation(); // Empêche le clic de remonter jusqu'à nav#menu
+    console.log("Clic détecté !");
+    sideBar.style.display = "none";
 }
